2333d8a2d84e76d2e24ecd5c54a39779db633ef1
Jash Vaidya <jashvaidya7x@gmail.com>
2017-07-11 23:19:55 -0400

    Major changes

--- js/main.js
+++ js/main.js
@@ -1,7 +1,22 @@
 function myFunction() {
     var x = document.getElementById('about');
+    var y = document.getElementById('contact');
+
     if (x.style.display === 'none') {
         x.style.display = 'block';
+        y.style.display = 'none';
+    } else {
+        x.style.display = 'none';
+    }
+}
+
+function mySecondFunction() {
+    var x = document.getElementById('contact');
+    var y = document.getElementById('about');
+
+    if (x.style.display === 'none') {
+        x.style.display = 'block';
+        y.style.display = 'none';
     } else {
         x.style.display = 'none';
     }
