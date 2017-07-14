69699038495332f3a3787a1291228bf6382e5f68
JashVaidya <jashvaidya7x@gmail.com>
2017-07-14 13:13:25 -0400

    major changes

--- js/main.js
+++ js/main.js
@@ -1,10 +1,12 @@
 function myFunction() {
     var x = document.getElementById('about');
     var y = document.getElementById('contact');
+    var z = document.getElementById('projects');
 
     if (x.style.display === 'none') {
         x.style.display = 'block';
         y.style.display = 'none';
+        z.style.display = 'none';
     } else {
         x.style.display = 'none';
     }
@@ -13,10 +15,26 @@ function myFunction() {
 function mySecondFunction() {
     var x = document.getElementById('contact');
     var y = document.getElementById('about');
+    var z = document.getElementById('projects');
 
     if (x.style.display === 'none') {
         x.style.display = 'block';
         y.style.display = 'none';
+        z.style.display = 'none';
+    } else {
+        x.style.display = 'none';
+    }
+}
+
+function myThirdFunction() {
+    var x = document.getElementById('projects');
+    var y = document.getElementById('about');
+    var z = document.getElementById('contact');
+
+    if (x.style.display === 'none') {
+        x.style.display = 'block';
+        y.style.display = 'none';
+        z.style.display = 'none';
     } else {
         x.style.display = 'none';
     }
