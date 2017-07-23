b850da85caa57025de8eb79caec73c6e9acddc68
Jash Vaidya <jashvaidya7x@gmail.com>
2017-07-22 21:00:43 -0400

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 function Function() {
     var x = document.getElementById('about');
     var y = document.getElementById('contact');
@@ -40,46 +39,3 @@ function ThirdFunction() {
         x.style.display = 'none';
     }
 }
-=======
-function myFunction() {
-    var x = document.getElementById('about');
-    var y = document.getElementById('contact');
-    var z = document.getElementById('projects');
-
-    if (x.style.display === 'none') {
-        x.style.display = 'block';
-        y.style.display = 'none';
-        z.style.display = 'none';
-    } else {
-        x.style.display = 'none';
-    }
-}
-
-function mySecondFunction() {
-    var x = document.getElementById('contact');
-    var y = document.getElementById('about');
-    var z = document.getElementById('projects');
-
-    if (x.style.display === 'none') {
-        x.style.display = 'block';
-        y.style.display = 'none';
-        z.style.display = 'none';
-    } else {
-        x.style.display = 'none';
-    }
-}
-
-function myThirdFunction() {
-    var x = document.getElementById('projects');
-    var y = document.getElementById('about');
-    var z = document.getElementById('contact');
-
-    if (x.style.display === 'none') {
-        x.style.display = 'block';
-        y.style.display = 'none';
-        z.style.display = 'none';
-    } else {
-        x.style.display = 'none';
-    }
-}
->>>>>>> origin/master
